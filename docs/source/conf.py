4fd5022812424b41ae2a0a2b9bf5ec618acdf167
guohui <guohui@mail.ustc.edu.cn>
2025-04-21 15:20:51 +0800

    add requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = "figures/LOGO.png"
+html_logo = "figures/logo.png"
 
 html_theme_options = {
     'logo_only': True
